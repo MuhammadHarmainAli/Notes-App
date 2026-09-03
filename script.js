1c47e5730dd124a4ac4963c9c093cf90997d41e4
muhammadharmainali <codebyharmain@gmail.com>
2026-09-04 02:26:19 +0500

    script.js update

--- script.js
+++ script.js
@@ -4,63 +4,118 @@ const contentInputElement = document.querySelector("#contentInput");
 const emptyStateElement = document.querySelector("#emptyState");
 const notesContainerElement = document.querySelector("#notesContainer");
 
-const notes = [];
+let notes = JSON.parse(localStorage.getItem("note")) || [];
 
-noteFormElement.addEventListener("submit", (e) => {
-  e.preventDefault();
+const getInputValue = () => {
+  return {
+    title: titleInputElement.value,
+    content: contentInputElement.value,
+  };
+};
 
-  const titleValue = titleInputElement.value;
-  const contentValue = contentInputElement.value;
-
+const createNote = (title, content) => {
   const now = new Date();
 
-  const note = {
+  return {
     id: Date.now(),
-    title: titleValue,
-    content: contentValue,
+    title: title,
+    content: content,
     date: now.toLocaleDateString("en-US", {
       month: "short",
       day: "numeric",
       year: "numeric",
     }),
-    time: now.toLocaleDateString("en-US", {
+    time: now.toLocaleTimeString("en-US", {
       hour: "2-digit",
       minute: "2-digit",
     }),
   };
+};
 
+const addNote = (note) => {
   notes.push(note);
+};
 
-  emptyStateElement.classList.add("hide");
+const resetForm = () => {
+  titleInputElement.value = "";
+  contentInputElement.value = "";
+};
 
+const updateEmptyState = () => {
+  if (notes.length === 0) {
+    emptyStateElement.classList.remove("hide");
+  } else {
+    emptyStateElement.classList.add("hide");
+  }
+};
+
+const renderNotes = () => {
   notesContainerElement.innerHTML = "";
 
   notes.forEach((note) => {
     const { id, title, content, date, time } = note;
 
-    noteFormElement.innerHTML = `
-    <article class="note-card">
-          <div class="note-card-header">
-            <h3 class="note-title">
-              ${title}
-            </h3>
-            <button class="delete-btn" data-id="123">
-              🗑
-            </button>
-          </div>
-          <p class="note-content">
-                          ${content}
-            <a href="#">read more</a>
-          </p>
-          <div class="note-footer">
-            <span class="note-date">
-              ${date}
-            </span>
-            <span class="note-time">
-              ${time}
-            </span>
-          </div>
-        </article>
-  `;
+    notesContainerElement.innerHTML += `
+      <article class="note-card">
+        <div class="note-card-header">
+          <h3 class="note-title">
+            ${title}
+          </h3>
+          <button class="delete-btn" data-id="${id}">
+            🗑
+          </button>
+        </div>
+        <p class="note-content">
+          ${content}
+          <a href="#">read more</a>
+        </p>
+        <div class="note-footer">
+          <span class="note-date">
+            ${date}
+          </span>
+          <span class="note-time">
+            ${time}
+          </span>
+        </div>
+      </article>
+    `;
+  });
+
+  updateEmptyState();
+};
+
+const saveNote = () => {
+  localStorage.setItem("note", JSON.stringify(notes));
+};
+
+const deleteNotes = (id) => {};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const { title, content } = getInputValue();
+
+  const note = createNote(title, content);
+
+  addNote(note);
+
+  resetForm();
+
+  saveNote();
+
+  renderNotes();
+};
+
+renderNotes();
+
+noteFormElement.addEventListener("submit", handleSubmit);
+
+const deleteButton = document.querySelectorAll(".delete-btn");
+
+deleteButton.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const checkedId = Number(btn.dataset.id);
+    
+    
   });
 });
